25b8978f2062cae7b7aa8687e9e7b559ad63ef95
vs3kulic <sekulicvajo@gmail.com>
2025-02-10 10:08:43 +0100

    structure cleanup

--- Fellowships/main.py
+++ Fellowships/main.py
@@ -1,4 +1,10 @@
 # Text-based Fellowship of the Ring game
+import sys
+import os
+
+# Add the parent directory to the system path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from Fellowships.game import Game
 
 game = Game()
