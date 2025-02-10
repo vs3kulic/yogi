95a15f8a2fd5578c68f729cf004da6b241070ce3
vs3kulic <sekulicvajo@gmail.com>
2025-02-10 13:49:55 +0100

    updated settings to include domain

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vs3kulic.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -105,6 +105,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
