fe461286af407bf8fc28c2b4d2f882152d9616d0
vs3kulic <sekulicvajo@gmail.com>
2025-02-10 18:23:51 +0100

    update html urls

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-from app import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('select_character/', views.select_character, name='select_character'),
     path('opponent_selected/', views.opponent_selected, name='opponent_selected'),
     path('coin_toss/', views.coin_toss, name='coin_toss'),
-    path('battle/', views.battle, name='battle'),
+    path('battle/', views.battle, name='battle')
 ]
