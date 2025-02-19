bbf0cb0ac9b2cb8fbbe81822124d31951352ce3a
vs3kulic <sekulicvajo@gmail.com>
2025-02-19 12:07:23 +0100

    added mysql db and sqlite dump file

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/vajosekulic/Documents/Programming/Fellowships/venv/bin/python3
+#!/Users/vajosekulic/Documents/Programming/Fellowships/venv/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
