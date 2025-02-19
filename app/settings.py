475f9fd7b6e79d10f2bc619fdaf8cbb56267e2e4
vs3kulic <sekulicvajo@gmail.com>
2025-02-19 22:20:51 +0100

    set up mysql db

--- app/settings.py
+++ app/settings.py
@@ -60,12 +60,29 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fellowship_db',  # Replace with your database name
+        'USER': 'vs3kulic',  # Replace with your MySQL username
+        'PASSWORD': 'mysql',  # Replace with your MySQL password
+        'HOST': 'localhost',          # Use '127.0.0.1' or the server IP if remote
+        'PORT': '3306',               # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
