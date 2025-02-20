1a2421b55dd692d32cdeee366e8a3d16ceec705e
vs3kulic <sekulicvajo@gmail.com>
2025-02-20 15:43:59 +0100

    update env variables

--- app/settings.py
+++ app/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME', default='fellowship_db'),
-        'USER': config('DB_USER', default='vs3kulic'),
+        'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306'),
