c2b59a3018ab1985576229bbdddf95c4d05884f3
vs3kulic <sekulicvajo@gmail.com>
2025-02-27 09:02:14 +0100

    added explicit statement for session storage in settings.py

--- app/settings.py
+++ app/settings.py
@@ -115,3 +115,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
