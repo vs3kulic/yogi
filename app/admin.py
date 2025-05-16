33345f8082d69e2496935522710b751520e33d48
vs3kulic <sekulicvajo@gmail.com>
2025-05-16 16:59:27 +0200

    fix: added site register to admin

--- app/admin.py
+++ app/admin.py
@@ -3,5 +3,8 @@ Description:
     This file is used to register models in the Django admin interface. 
     It allows for easy management of the models through the admin panel.
 """
-
 from django.contrib import admin
+from .models import YogaClass
+
+# Register the YogaClass model
+admin.site.register(YogaClass)
