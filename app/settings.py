4977548eab370c821d5be6cea4fb39c177baaebe
vs3kulic <sekulicvajo@gmail.com>
2025-05-25 01:57:46 +0200

    fix(git): added db_name

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,club.bekindstudio.at').split(',')
 
 # Database configuration
 DATABASES = {
