f003d7f5094d92a662236bb44e48897e2d3e0244
vs3kulic <sekulicvajo@gmail.com>
2025-05-25 02:04:38 +0200

    fix(git): add allowed_host

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,club.bekindstudio.at').split(',')
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1,club.bekindstudio.at,www.club.bekindstudio.at'
+).split(',')
 
 # Database configuration
 DATABASES = {
