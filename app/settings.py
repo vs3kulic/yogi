f63cb58b1d550af98ccc2c66f1cc9549f7d24e74
vs3kulic <sekulicvajo@gmail.com>
2025-06-11 08:31:39 +0200

    chore(settings): removed unused redis dependency

--- app/settings.py
+++ app/settings.py
@@ -221,11 +221,8 @@ TEMPLATES = [
 ]
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
     }
 }
